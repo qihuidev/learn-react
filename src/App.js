60b8e2ea2a06a4ffd06c78082640f4a41e7754a0
liu Qihui <liuqihui@liudeMacBook-Pro.local>
2020-08-19 16:04:39 +0800

    新增组件

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Fragment } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import store from './store';
 import Header from './common/header/index';
+import Home from './pages/home';
+import Login from './pages/login';
+import Write from './pages/write';
+import Detail from './pages/detail';
 
 import GlobalStyle from './style';
 import GlobalStyleFont from './static/iconfont/iconfont';
@@ -13,7 +18,13 @@ function App() {
       <GlobalStyle />
       <GlobalStyleFont />
       <Provider store={store}>
-        <Header />
+        <BrowserRouter>
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/write" component={Write} />
+          <Route exact path="/detail/:id" component={Detail} />
+        </BrowserRouter>
       </Provider>
     </Fragment>
   );
