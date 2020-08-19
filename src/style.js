d6c555fa3dfe29d8ba12a3b2f2d19a5f16ae3857
liu Qihui <liuqihui@liudeMacBook-Pro.local>
2020-08-19 16:06:14 +0800

    新增 a z标签默认样式

--- src/style.js
+++ src/style.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
-const GlobalStyle =  createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
@@ -43,6 +43,9 @@ const GlobalStyle =  createGlobalStyle`
   table {
     border-collapse: collapse;
     border-spacing: 0;
+  }
+  a {
+    text-decoration: none;
   }
 `;
 
